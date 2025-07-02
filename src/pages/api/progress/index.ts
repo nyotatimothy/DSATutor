830a6fa6488aedf7aef97b29cd39b4a5399f3f89
Timothy Nyota <timothynyota@Timothys-MacBook-Air.local>
2025-07-02 22:59:54 +0300

    Add Progress API with comprehensive tests and update report.html - 90% success rate

---
+++ src/pages/api/progress/index.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { ProgressController } from '../../../controllers/progressController'
+import { authenticateToken } from '../../../middlewares/auth'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Apply authentication middleware
+  authenticateToken(req as any, res, () => {
+    // Continue with the request
+    switch (req.method) {
+      case 'GET':
+        return ProgressController.getUserProgress(req, res)
+      
+      case 'POST':
+        return ProgressController.createProgress(req, res)
+      
+      default:
+        return res.status(405).json({
+          success: false,
+          error: 'Method not allowed'
+        })
+    }
+  })
+} 
---
+++ src/pages/api/progress/stats.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { ProgressController } from '../../../controllers/progressController'
+import { authenticateToken } from '../../../middlewares/auth'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Apply authentication middleware
+  authenticateToken(req as any, res, () => {
+    // Continue with the request
+    switch (req.method) {
+      case 'GET':
+        return ProgressController.getProgressStats(req, res)
+      
+      default:
+        return res.status(405).json({
+          success: false,
+          error: 'Method not allowed'
+        })
+    }
+  })
+} 
